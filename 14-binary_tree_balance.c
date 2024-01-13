12edb029b7dcc6614502cc4732986981fb94c80a
aishalieva0 <aysalieva6@gmal.com>
2024-01-13 15:04:22 -0800

    error fixing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
 
-    h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+    h_left = 1 + binary_tree_height(tree->left);
+    h_right = 1 + binary_tree_height(tree->right);
 
     return (h_left > h_right ? h_left : h_right);
 }
